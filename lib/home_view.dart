a5ec849e1e204a39402d84f55b1bb79a1abaf049
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-19 13:37:02 -0300

    add function _calculate in HomeView

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -6,6 +6,37 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  TextEditingController _alcoholController = TextEditingController();
+  TextEditingController _gasolineController = TextEditingController();
+  String _choiceResult = '';
+
+  void _calculate() {
+    double? alcoholPrice = double.tryParse(_alcoholController.text);
+    double? gasolinePrice = double.tryParse(_gasolineController.text);
+
+    if(alcoholPrice == null || gasolinePrice == null) {
+      setState(() {
+        _choiceResult = 'Número inválido';
+      });
+    } else {
+      if(alcoholPrice/gasolinePrice >= 0.7){
+        setState(() {
+          _choiceResult = 'Melhor abastecer com gasolina!';
+        });
+      } else{
+        setState(() {
+          _choiceResult = 'Melhor abastecer com álcool!';
+        });
+      }
+      _cleanField();
+    }
+  }
+
+  void _cleanField(){
+    _alcoholController.text = '';
+    _gasolineController.text = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,10 +44,11 @@ class _HomeViewState extends State<HomeView> {
         title: Text('Álcool ou gasolina'),
         backgroundColor: Colors.blue,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
             Padding(
               padding: const EdgeInsets.only(bottom: 32),
               child: Image.asset('assets/images/logo.png'),
@@ -25,20 +57,48 @@ class _HomeViewState extends State<HomeView> {
               padding: const EdgeInsets.only(bottom: 10),
               child: Text(
                 'Saiba qual é a melhor para abastecimento do seu carro',
-                style: TextStyle(fontSize: 25, color: Colors.black),
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
             ),
-            TextField(),
-            TextField(),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Preço Álcool, ex: 1.59'),
+              style: TextStyle(fontSize: 18),
+              controller: _alcoholController,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration:
+                  InputDecoration(labelText: 'Preço Gasolina, ex: 3.59'),
+              style: TextStyle(fontSize: 18),
+              controller: _gasolineController,
+            ),
             Padding(
               padding: EdgeInsets.only(top: 10),
               child: RaisedButton(
-                onPressed: () {},
-                child: Text('Calcular'),
+                onPressed: _calculate,
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  'Calcular',
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
-          ],
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                _choiceResult,
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ]),
         ),
       ),
     );
